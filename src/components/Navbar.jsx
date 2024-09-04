1313475d8d5e7467bae09ff04d4760d19507beca
sammie slootman <sammie.slootman@gmail.com>
2024-09-04 22:55:20 +0200

    made sure the theme worked by changing color for  navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,29 @@
 // src/components/Navbar.jsx
 import React from "react";
 import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 
 const Navbar = () => {
+  const theme = useTheme(); // Access the theme
+
   return (
-    <AppBar position="static">
+    <AppBar
+      position="static"
+      sx={{ backgroundColor: theme.palette.secondary.main }}
+    >
       <Toolbar>
         <Typography variant="h6" sx={{ flexGrow: 1 }}></Typography>
         <Button color="inherit" href="#home">
-          Hem
+          Home
         </Button>
         <Button color="inherit" href="#about">
-          Om Mig
+          About Me
         </Button>
         <Button color="inherit" href="#projects">
-          Projekt
+          Projects
         </Button>
         <Button color="inherit" href="#contact">
-          Kontakta
+          Contact
         </Button>
         <Button color="inherit" href="#cv">
           CV
